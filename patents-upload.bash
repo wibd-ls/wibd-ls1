c55b43a5d97a54fe8f79e25c13d7bf976b34d637
swathipuskur <swathipuskur@gmail.com>
2018-09-28 14:34:03 -0400

    Tested script

--- patents-upload.bash
+++ patents-upload.bash
@@ -7,21 +7,13 @@
 
 
 #install cli from aws 
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+#curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 
 #credentials
 
 export AWS_ACCESS_KEY_ID=secretkey
 export AWS_SECRET_ACCESS_KEY=secretkey
-export AWS_DEFAULT_REGION=us-east-2
+export AWS_DEFAULT_REGION=us-east-1
+/usr/local/bin/./aws s3 sync patents s3://wibd-ls1/
 
 
- 
-
-#cron job
-
-crontab -l > uploadcron
-echo "0 2 * * * root aws s3 sync “.\patents\” s3://wibd-ls1/ " >> uploadcron
-
-#install new cron file
-crontab uploadcron
