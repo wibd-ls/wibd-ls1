c506ea3823ec20e97ea9d69deef2390dc49aed5c
kanan-ch <36203688+kanan-ch@users.noreply.github.com>
2018-10-07 11:28:01 -0400

    Create EnvironmentConstants.java

---
+++ Text_Detection/src/main/java/wibd/ls/ml/util/EnvironmentConstants.java
@@ -0,0 +1,10 @@
+package wibd.ls.ml.util;
+
+import com.amazonaws.regions.Regions;
+
+public class EnvironmentConstants {
+	public static final String accessKeyId = "XXXXX";
+	public static final String secretKeyId = "XXXXX";
+	public static final Regions region = Regions.US_EAST_2;
+	public static final String BUCKET_NAME = "XXXX";
+}
